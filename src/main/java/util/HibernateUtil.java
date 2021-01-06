ab7063d4062b7edef0cd39f6e63a3b441d8d57e4
Alex <bonus_me@mail.ru>
2021-01-06 15:23:48 +0300

    Using heroku first time

--- src/main/java/util/HibernateUtil.java
+++ src/main/java/util/HibernateUtil.java
@@ -19,9 +19,10 @@ public class HibernateUtil {
                 Configuration configuration = new Configuration();
                 Properties settings = new Properties();
                 settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/webtest?serverTimezone=UTC");
-                settings.put(Environment.USER, "root");
-                settings.put(Environment.PASS, "JDBC24Java");
+                settings.put(Environment.URL, "jdbc:mysql://n2o93bb1bwmn0zle.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:" +
+                        "3306/snjv3tbhkdt9rr96?serverTimezone=UTC&sslmode=require");
+                settings.put(Environment.USER, "vyofr0p29bofcxtj");
+                settings.put(Environment.PASS, "yf7llclhc3pwkcol");
                 settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
                 settings.put(Environment.SHOW_SQL, "true");
                 settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
